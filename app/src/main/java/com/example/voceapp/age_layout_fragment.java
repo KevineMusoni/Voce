387566ffd2de3dbd079fd798f8a464a8403375e7
KevineMusoni <kevinenicky@gmail.com>
2022-01-04 10:24:52 +0300

    ...

--- app/src/main/java/com/example/voceapp/age_layout_fragment.java
+++ app/src/main/java/com/example/voceapp/age_layout_fragment.java
@@ -1,18 +1,45 @@
 package com.example.voceapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
 
 public class age_layout_fragment extends Fragment {
 
+    RadioGroup radioGroup;
+    RadioButton radioButton;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
-        ViewGroup root = (ViewGroup) inflater.inflate(R.layout.age_layout_fragment, container,false);
 
-        return root;
+        View view = (ViewGroup) inflater.inflate(R.layout.age_layout_fragment, container,false);
+
+        radioGroup = view.findViewById(R.id.radiogroup);
+
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener(){
+            public void onCheckedChanged(RadioGroup group, int checkedId){
+                switch (checkedId){
+                    case R.id.b_age:
+//                        open below age read activity
+                        Intent intent = new Intent(getActivity(), below.class);
+                        startActivity(intent);
+                    case R.id.a_age:
+//                        open above age read activity
+                        Intent intentTwo = new Intent(getActivity(), above.class);
+                        startActivity(intentTwo);
+                }
+            }
+        });
+
+
+        return view;
     }
 }
--- app/src/main/java/com/example/voceapp/type_layout_fragment.java
+++ app/src/main/java/com/example/voceapp/type_layout_fragment.java
@@ -4,15 +4,25 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import androidx.fragment.app.Fragment;
 
 public class type_layout_fragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
-        ViewGroup root = (ViewGroup) inflater.inflate(R.layout.type_layout_fragment, container,false);
 
-        return root;
+
+//        ViewGroup root = (ViewGroup) inflater.inflate(R.layout.type_layout_fragment, container,false);
+
+        View view = inflater.inflate(R.layout.type_layout_fragment, container, false);
+        Spinner spinner = view.findViewById(R.id.spinner);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getActivity(), R.array.types, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
+        return view;
     }
 }
